d205e5b99f5e414e7363fbb54fbb509f5b3e3a40
straysheep-dev <bah@straysheep.dev>
2025-06-17 21:48:22 -0700

    Update build scripts

--- ubuntu/ubuntu-cloudinit/build-ubuntu-22.04-desktop.sh
+++ ubuntu/ubuntu-cloudinit/build-ubuntu-22.04-desktop.sh
@@ -3,7 +3,13 @@
 # Build the VM
 # Change to PACKER_LOG=1 for debug output
 PACKER_LOG=0 \
-packer build \
+
+actions='validate build'
+
+for action in $actions
+do
+    packer "$action" \
     -var "iso_storage_path=${HOME}/iso/ubuntu-22.04.4-live-server-amd64.iso" \
     -var-file="22.04-desktop.pkrvars.hcl" \
     .
+done
--- ubuntu/ubuntu-cloudinit/build-ubuntu-24.04-server.sh
+++ ubuntu/ubuntu-cloudinit/build-ubuntu-24.04-server.sh
@@ -3,7 +3,13 @@
 # Build the VM
 # Change to PACKER_LOG=1 for debug output
 PACKER_LOG=0 \
-packer build \
+
+actions='validate build'
+
+for action in $actions
+do
+    packer "$action" \
     -var "iso_storage_path=${HOME}/iso/ubuntu-24.04.2-live-server-amd64.iso" \
     -var-file="24.04-server.pkrvars.hcl" \
     .
+done
